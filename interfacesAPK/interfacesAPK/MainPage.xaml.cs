007a3b1e1da221f0a0b144797d455a9f638483c7
Devil-bit123 <80175560+Devil-bit123@users.noreply.github.com>
2022-08-29 00:19:48 -0500

    Implementacion del APK para visualizar datos desde remoto

---
+++ interfacesAPK/interfacesAPK/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using interfacesAPK.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace interfacesAPK
+{
+    public partial class MainPage : ContentPage
+    {
+        private string URL = "https://interfaceselec.azurewebsites.net/api/sensors";
+
+        HttpClient cliente = new HttpClient();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            string contenido= await cliente.GetStringAsync(URL);
+            IEnumerable<userModel> lista  = JsonConvert.DeserializeObject<IEnumerable<userModel>>(contenido);
+            collection.ItemsSource= new ObservableCollection<userModel>(lista);
+            base.OnAppearing();
+        }
+    }
+}
---
+++ interfacesAPK/interfacesAPK/Models/userModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace interfacesAPK.Models
+{
+    public class userModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange([CallerMemberName] string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+
+        private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; OnPropertyChange(); }
+		}
+
+		private string valorVoltaje;
+
+		public string ValorVoltaje
+		{
+			get { return valorVoltaje; }
+			set { valorVoltaje = value; OnPropertyChange(); }
+		}
+
+		private string valorTemperatura;
+
+		public string ValorTemperatura
+		{
+			get { return valorTemperatura; }
+			set { valorTemperatura = value; OnPropertyChange(); }
+		}
+
+		private string valorDistancia;
+
+		public string ValorDistancia
+		{
+			get { return valorDistancia; }
+			set { valorDistancia = value; OnPropertyChange(); }
+		}
+
+		private string fecha;	
+
+
+        public string Fecha
+		{
+			get { return fecha; }
+			set { fecha = value; OnPropertyChange(); }
+		}
+
+
+	}
+}
